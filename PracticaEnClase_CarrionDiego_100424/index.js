775cca290ede2cf8c1bc24a5a4a9c1d745813d0f
Diego Carrion Portilla <dcportilla2003@gmail.com>
2024-04-13 12:51:03 -0500

    Implementacion de la calculadora
    Primera prueba de la calculadora con solo 6 botones

---
+++ PracticaEnClase_CarrionDiego_100424/index.js
@@ -0,0 +1,31 @@
+// Función para mostrar u ocultar ejemplos al hacer clic en el botón
+function toggleExamples(targetId) {
+    var target = document.getElementById(targetId);
+    target.classList.toggle("show");
+}
+
+// Agregar evento de clic a todos los botones con la clase toggleButton
+var toggleButtons = document.querySelectorAll(".toggleButton");
+toggleButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        var targetId = button.getAttribute("data-target");
+        toggleExamples(targetId);
+    });
+});
+
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
--- PracticaEnClase_CarrionDiego_100424/indexjs.js
+++
@@ -1,9 +0,0 @@
-// JavaScript source code
-function toggleEjemplos(operacion) {
-    var ejemplos = document.getElementById('ejemplos' + operacion);
-    if (ejemplos.style.display === 'none') {
-        ejemplos.style.display = 'block';
-    } else {
-        ejemplos.style.display = 'none';
-    }
-}
