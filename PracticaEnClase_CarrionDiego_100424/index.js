716cd30050809f887bae29af406600eafa29c35f
Diego Carrion Portilla <dcportilla2003@gmail.com>
2024-04-14 14:32:52 -0500

    Correccion de errores
    La calculadora no mostraba los datos pero era problema del navegador almacenando el js antiguo y no le nuevo. Error arreglado

--- PracticaEnClase_CarrionDiego_100424/index.js
+++ PracticaEnClase_CarrionDiego_100424/index.js
@@ -1,29 +1,9 @@
-// Función para mostrar u ocultar ejemplos al hacer clic en el botón
+
 function toggleExamples(targetId) {
     var target = document.getElementById(targetId);
     target.classList.toggle("show");
 }
 
-// Función para manejar eventos de clic en los botones de la calculadora
-function handleButtonClick(value) {
-    var display = document.getElementById('display');
-}
-
-// Función para borrar el contenido de la pantalla
-function clearDisplay() {
-    document.getElementById('display').value = '';
-}
-
-// Función para calcular el resultado de la operación
-function calculate() {
-    try {
-        document.getElementById('display').value = eval(document.getElementById('display').value);
-    } catch (error) {
-        document.getElementById('display').value = 'Error';
-    }
-}
-
-// Agregar evento de clic a todos los botones con la clase toggleButton
 var toggleButtons = document.querySelectorAll(".toggleButton");
 toggleButtons.forEach(function (button) {
     button.addEventListener("click", function () {
@@ -32,11 +12,19 @@ toggleButtons.forEach(function (button) {
     });
 });
 
-// Agregar evento de clic a todos los botones de la calculadora
-var calculatorButtons = document.querySelectorAll(".calculator .buttons button");
-calculatorButtons.forEach(function (button) {
-    button.addEventListener("click", function () {
-        handleButtonClick(button.textContent);
-    });
-});
 
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
